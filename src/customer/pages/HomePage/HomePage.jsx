fc3daf005f49ceb75d30f3459b744ca7c36ffe73
prajapatijay03 <jayp37679@gmail.com>
2024-01-10 12:43:36 +0530

    js

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+function HomePage() {
+    return (
+        <div>
+            <MainCarousel />
+
+            <div>
+                other section
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
