a376b19ba383924a6ad632d70a1ea5f5e6532158
prajapatijay03 <jayp37679@gmail.com>
2024-01-13 17:17:37 +0530

    chnages

---
+++ src/customer/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <Grid className='bg-black text-white text-center
+        mt-10'
+        container
+        sx={{bgcolor:"black", color:"white", py:3}}
+      >
+
+        <Grid></Grid>
+
+      </Grid>
+
+
+    </div>
+  )
+}
+
+export default Footer
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/means_kurta.js'
 
-function HomePage() {
+const HomePage = () => {
     return (
         <div>
             <MainCarousel />
 
-            <div>
-                other section
+            <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10 '>
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"} />
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Mean's Shoes"} />
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Mea's Shirt"}  />
+                <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Saree"} /> <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Dress"} />
             </div>
         </div>
     )
